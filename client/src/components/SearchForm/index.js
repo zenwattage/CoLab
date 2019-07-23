50a6412ae0376a5231b32698d7c843938bb76790
Russ <russ.dermody@gmail.com>
2019-07-23 08:37:22 -0700

    final commit

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -4,36 +4,6 @@ import axios from 'axios';
 import Filter from "../Filter/Filter";
 import "./style.css";
 
-<<<<<<< HEAD
-// Using the datalist element we can  autofill suggestions based on the props.breeds array
-function SearchForm(props) {
-  return (
-    <form className="search">
-      <div className="form-group">
-        <label htmlFor="breed">Profession:</label>
-        {/* <input
-          value={props.search}
-          onChange={props.handleInputChange}
-          name=""
-          list=""
-          type="text"
-          className="form-control"
-          placeholder="Type in a dog breed to begin"
-          id="breed"
-        />
-        <datalist id="">
-          {props.breeds.map(breed => (
-            <option value={breed} key={breed} />
-          ))}
-        </datalist> */}
-        <Button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-          Search
-          </Button>
-      </div>
-    </form>
-  );
-}
-=======
 // Using the datalist element we can create autofill suggestions based on the props.breeds array
 export default class SearchForm extends Component {
   state = {
@@ -111,7 +81,6 @@ export default class SearchForm extends Component {
           {/* <h1 className="title">Welcome to CoLab!</h1>
           <p>Please fill out the registration form to sign-up.</p>
           <p>You will complete your profile on the next page.</p> */}
->>>>>>> master
 
           <form onSubmit={this.handleSubmit}>
 
