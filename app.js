c313698a70cc1fe7f5f3255ae28853c192b6b022
Russ <russ.dermody@gmail.com>
2019-07-31 18:34:29 -0700

    Updated seed files

--- app.js
+++ app.js
@@ -19,58 +19,6 @@ var db = require("./models");
 //database connection
 mongoose.connect("mongodb://localhost/newColabDB", { useNewUrlParser: true });
 
-// // ///////////////////////
-// // Schema instantiation //
-// // ///////////////////////
-// db.ballet.create({
-//   userId: "test",
-//   properties: { pointe: true },
-//   images: ["one", "two", "three"],
-//   videos: ["one", "two", "three"]
-// });
-// db.latin.create({
-//   userId: "test",
-//   properties: { freestyle: true },
-//   images: ["one", "two", "three"],
-//   videos: ["one", "two", "three"]
-// });
-// db.hipHop.create({
-//   userId: "test",
-//   properties: { freestyle: true },
-//   images: ["one", "two", "three"],
-//   videos: ["one", "two", "three"]
-// });
-// db.contemporary.create({
-//   userId: "test",
-//   properties: { freestyle: true },
-//   images: ["one", "two", "three"],
-//   videos: ["one", "two", "three"]
-// });
-// db.portrait.create({
-//   userId: "test",
-//   properties: { camera: true },
-//   images: ["one", "two", "three"],
-//   videos: ["one", "two", "three"]
-// });
-// db.street.create({
-//   userId: "test",
-//   properties: { camera: true },
-//   images: ["one", "two", "three"],
-//   videos: ["one", "two", "three"]
-// });
-// db.motion.create({
-//   userId: "test",
-//   properties: { camera: true },
-//   images: ["one", "two", "three"],
-//   videos: ["one", "two", "three"]
-// });
-// db.landscape.create({
-//   userId: "test",
-//   properties: { camera: true },
-//   images: ["one", "two", "three"],
-//   videos: ["one", "two", "three"]
-// });
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
