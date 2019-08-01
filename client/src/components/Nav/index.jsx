920b47aaa535e4d54a9b85ffaf8e4389e62c0645
Jordynn Otto <j_taylor92@yahoo.com>
2019-07-31 20:45:50 -0700

    navbar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,45 +1,26 @@
 import React from "react";
 import "./style.css";
-import Navbar from 'react-bootstrap/Navbar';
-
-// CANNOT GET THIS TO WORK BUT WANT TO
-function Nav() {
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+function NavBarStuff() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#"> <img
-        alt=""
-        src=""
-        width="20"
-        height="30"
-        className="d-inline-block align-top"
-      />
-        {' CoLab'}
+    <Navbar bg="light">
+      <Navbar.Brand className="Navbar-brand" href="#">
+        <img
+          alt=""
+          src="logo5.png"
+          width="20"
+          height="30"
+          className="d-inline-block align-top"
+        />
+        {" CoLab"}
       </Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="#">Home</Nav.Link>
-        <Nav.Link href="#">Link</Nav.Link>
+        <Nav.Link href="/profile">Profile</Nav.Link>
+        <Nav.Link href="/search">Search</Nav.Link>
+        <Nav.Link href="#">Logout</Nav.Link>
       </Nav>
     </Navbar>
-  )
+  );
 }
-export default Nav;
-
-// function Nav() {
-//   return (
-//     <nav className="navbar">
-//       <a className="navbar-brand-name" href="/">
-//         CoLab
-//       </a>
-//       <a className="navbar-brand" href="/signup">
-//         Sign Up
-//       </a>
-//       <a className="navbar-brand" href="/profile">
-//         Profile
-//       </a>
-//       <a className="navbar-brand" href="/search">
-//         Search
-//       </a>
-//     </nav>
-//   )}
-// export default Nav;
-
+export default NavBarStuff;
