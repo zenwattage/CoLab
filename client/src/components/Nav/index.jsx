afba62b8a64da9639a381b72679f7598223fdedf
Jordynn Otto <j_taylor92@yahoo.com>
2019-07-31 20:17:23 -0700

    profile page

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,38 +1,45 @@
 import React from "react";
-import "./style.css"; 
-// import Navbar from 'react-bootstrap/Navbar';
+import "./style.css";
+import Navbar from 'react-bootstrap/Navbar';
+
+// CANNOT GET THIS TO WORK BUT WANT TO
+function Nav() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#"> <img
+        alt=""
+        src=""
+        width="20"
+        height="30"
+        className="d-inline-block align-top"
+      />
+        {' CoLab'}
+      </Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#">Home</Nav.Link>
+        <Nav.Link href="#">Link</Nav.Link>
+      </Nav>
+    </Navbar>
+  )
+}
+export default Nav;
 
 // function Nav() {
 //   return (
-//   <Navbar bg="light" variant="light">
-//     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-//     <Nav className="mr-auto">
-//       <Nav.Link href="#home">Home</Nav.Link>
-//       <Nav.Link href="#signup">Features</Nav.Link>
-//       <Nav.Link href="#search">Pricing</Nav.Link>
-//       <Nav.Link href="#profile">Pricing</Nav.Link>
-//     </Nav>
-//   </Navbar>
-// )}
-
+//     <nav className="navbar">
+//       <a className="navbar-brand-name" href="/">
+//         CoLab
+//       </a>
+//       <a className="navbar-brand" href="/signup">
+//         Sign Up
+//       </a>
+//       <a className="navbar-brand" href="/profile">
+//         Profile
+//       </a>
+//       <a className="navbar-brand" href="/search">
+//         Search
+//       </a>
+//     </nav>
+//   )}
 // export default Nav;
 
-function Nav() {
-  return (
-    <nav className="navbar">
-      <a className="navbar-brand" href="/">
-        CoLab
-      </a>
-      <a className="navbar-brand" href="/signup">
-        Sign Up
-      </a>
-      <a className="navbar-brand" href="/profile">
-        Profile
-      </a>
-      <a className="navbar-brand" href="/search">
-        Search
-      </a>
-    </nav>
-  )}
-
-export default Nav;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,6 +6,8 @@ import axios from "axios";
 import Wrapper from "../components/Wrapper/index";
 import Footer from "../components/Footer/index";
 import Form from "react-bootstrap/Form";
+import "./style.css";
+import logo from "../logo5.png";
 
 class Profile extends Component {
   // State to store images of work
@@ -71,72 +73,74 @@ class Profile extends Component {
         <Wrapper>
           <Row>
             <Col>
-              <h1>Welcome, {this.state.firstName}</h1>
+              <h1 className="welcome">Welcome, {this.state.firstName}</h1>
             </Col>
           </Row>
 
-        <div>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>First name</Form.Label>
-                <Form.Control value={this.state.firstName} name="firstName" onChange={this.handleChange} type="name" placeholder="Enter first name" />
-              </Form.Group>
+          <div>
+            <Form onSubmit={this.handleSubmit}>
+              <div className="personalinfo">
+                <h3 className="personalinfotitle">Personal Info</h3>
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridAddress2">
+                    <p>Profile image:</p>
+                    <img src={logo} alt="Logo" width="100px" />
+                    {/* <Form.Label>Profile image</Form.Label>
+                  <Form.Control value={this.state.imageUrl} name="imageUrl" onChange={this.handleChange} /> */}
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridEmail">
+                    <p>Name: Jordynn Otto</p>
+                    {/* <Form.Label>First name</Form.Label>
+                    <Form.Control value={this.state.firstName} name="firstName" onChange={this.handleChange} type="name" placeholder="Enter first name" /> */}
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridPassword">
+                    {/* <Form.Label>Last name</Form.Label>
+                    <Form.Control value={this.state.lastName} name="lastName" onChange={this.handleChange} type="name" placeholder="Enter last name" /> */}
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridEmail">
+                    <p>Email: j_taylor92@yahoo.com</p>
+                    {/* <Form.Label>Email</Form.Label>
+                    <Form.Control value={this.state.email} name="email" onChange={this.handleChange} type="email" placeholder="Enter email" /> */}
+                  </Form.Group>
+                </Form.Row>
+              </div>
 
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control value={this.state.lastName} name="lastName" onChange={this.handleChange} type="name" placeholder="Enter last name" />
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control value={this.state.email} name="email" onChange={this.handleChange} type="email" placeholder="Enter email" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control value={this.state.password} name="password" onChange={this.handleChange} type="password" placeholder="Create password" />
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Bio</Form.Label>
-              <Form.Control value={this.state.bio} name="bio" onChange={this.handleChange} type="text" as="textarea" rows="6" placeholder="Tell us something interesting..." />
-            </Form.Group>
-
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridAddress2">
-                <Form.Label>Profile image</Form.Label>    
-                  <Form.Control value={this.state.imageUrl} name="imageUrl" onChange={this.handleChange} />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridAddress2">
-                <Form.Label>LinkedIn</Form.Label>
-                <Form.Control value={this.state.linkedin} name="linkedin" onChange={this.handleChange} />
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridAddress2">
-                <Form.Label>Instagram</Form.Label>
-                <Form.Control value={this.state.instagram} name="instagram" onChange={this.handleChange} />
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridAddress2">
-                <Form.Label>Other</Form.Label>
-                <Form.Control value={this.state.other} name="other" onChange={this.handleChange} placeholder="Anything else?" />
-              </Form.Group>
-            </Form.Row>
-            <br />
-            <button className="submitbutton">Submit</button>
-
-          </Form>
-        </div>
-
-      </Wrapper>
-      <Footer />
-    </Fragment >
+              <div className="personalinfo">
+                <h3 className="personalinfotitle">Portfolio</h3>
+                {/* <Form.Row>  */}
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                  <p>Bio: I love to dance, it is the best thing ever.</p>
+                  {/* <Form.Label>Bio</Form.Label>
+                  <Form.Control value={this.state.bio} name="bio" onChange={this.handleChange} type="text" as="textarea" rows="6" placeholder="Tell us something interesting..." /> */}
+                </Form.Group>
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridAddress2">
+                    <p>Linkedin: www.linkedin.com/jottobot</p>
+                    {/* <Form.Label>LinkedIn</Form.Label>
+                    <Form.Control value={this.state.linkedin} name="linkedin" onChange={this.handleChange} /> */}
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridAddress2">
+                    <p>Instagram: @jottobot</p>
+                    {/* <Form.Label>Instagram</Form.Label>
+                    <Form.Control value={this.state.instagram} name="instagram" onChange={this.handleChange} /> */}
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridAddress2">
+                    <p>Other: www.youtube.com/jottobot</p>
+                    {/* <Form.Label>Other</Form.Label>
+                    <Form.Control value={this.state.other} name="other" onChange={this.handleChange} placeholder="Anything else?" /> */}
+                  </Form.Group>
+                </Form.Row>
+                <br />
+                <div className="profilebuttons">
+                  <button className="profilebutton">Edit</button>
+                  <button className="profilebutton">Submit</button>
+                </div>
+              </div>
+            </Form>
+          </div>
+        </Wrapper>
+        <Footer />
+      </Fragment >
     );
   }
 };
