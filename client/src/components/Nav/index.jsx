78bfddc58c46c1e50ee5ba8cb3f8367ee254dfbd
zenwattage <zenwattage@gmail.com>
2019-08-03 12:55:42 -0700

    commit navbar logout button

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -4,28 +4,6 @@ import { Redirect } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/Nav";
-<<<<<<< HEAD
-function NavBarStuff() {
-  return (
-    <Navbar bg="light">
-      <Navbar.Brand className="Navbar-brand" href="#">
-        <img
-          alt=""
-          src="./logo5.png"
-          width="20"
-          height="30"
-          className="d-inline-block align-top"
-        />
-        {" CoLab"}
-      </Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="/profile">Profile</Nav.Link>
-        <Nav.Link href="/search">Search</Nav.Link>
-        <Nav.Link href="/logout">Logout</Nav.Link>
-      </Nav>
-    </Navbar>
-  );
-=======
 import logo from "./logo5.png";
 import axios from "axios";
 
@@ -75,5 +53,4 @@ export default class NavBarStuff extends Component {
       </Navbar>
     );
   }
->>>>>>> 7ed148277d0bed67b6d81e52c5153127682d7d3b
 }
