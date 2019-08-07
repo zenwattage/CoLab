4795b762c3c18bcd692487f762dc5f6dcd9f822a
Russ <russ.dermody@gmail.com>
2019-08-07 12:33:54 -0700

    Trying to get new search working..

--- routes/api/searchRoute.js
+++ routes/api/searchRoute.js
@@ -1,8 +1,12 @@
+//? This file's purpose is:
+//? To control the logic that runs
+//? when USER pings "/search"
 const express = require("express");
 const router = express.Router();
 const userController = require("../../controllers/userController");
+const db = require("../../models");
 
-// * /api/search
+//* /api/search
 router.post("/search", function(req, res) {
   console.log("Checking in from searchRoute");
   //* THIS CODE BUILDS THE QUERY VARIABLES
@@ -22,6 +26,15 @@ router.post("/search", function(req, res) {
   }
   console.log("Profession Query: " + professionQuery);
   console.log("Talent Query: " + talentQuery);
+
+  // * THIS CODE PERFORMS THE DB SEARCH
+
+  db.users
+    .find({ userProfession: "Dancer" })
+    // .find(req.query)
+    // .sort({ date: -1 })
+    .then(QueryRes => res.json(QueryRes))
+    .catch(err => res.status(422).json(err));
 
   // userController.search;
 });
